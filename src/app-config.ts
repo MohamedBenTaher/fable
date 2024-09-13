43867b3ede6da316ae9d68e1dda9e036d92d877f
Mohamed Ben Taher <mbentaher1998@gmail.com>
2024-09-13 01:50:25 +0100

    chore: Add .env to .gitignore and update npm dependencies

---
+++ src/app-config.ts
@@ -0,0 +1,3 @@
+export const applicationName = "Fable";
+
+export const afterLoginUrl = "/dashboard";
---
+++ src/components/nabvar.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import MaxWidthWrapper from "./max-width-wrapper";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+
+function Navbar() {
+  return (
+    <div className=" sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-md transition-all ">
+      <MaxWidthWrapper>
+        <div className="flex h-14 items-center justify-between border-b border-zinc-200">
+          <Link href="/" className="flex z-40 font-semibold">
+            <span className="text-2xl text-gray-900">Fable.</span>
+          </Link>
+          <div className="hidden items-center space-x-4 sm:flex">
+            <Link
+              className={buttonVariants({ size: "sm", variant: "ghost" })}
+              href="/dashboard"
+            >
+              Pricing
+            </Link>
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </div>
+  );
+}
+
+export default Navbar;
