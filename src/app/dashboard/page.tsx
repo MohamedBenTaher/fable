4e486e53a0d98847708ef160f5c22709995865b7
Mohamed Ben Taher <mbentaher1998@gmail.com>
2024-09-18 08:06:31 +0100

    dasbhoard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,16 @@
 import { getCurrentUser } from "@/lib/session";
 import React from "react";
+import Dashbaord from "@/components/dashboard";
 
 async function page() {
   const user = await getCurrentUser();
   console.log("user", user);
-  return <div>hello</div>;
+  return (
+    <div>
+      <Dashbaord />
+      
+    </div>
+  );
 }
 
 export default page;
